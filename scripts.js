a6533c1a0dc2d878bfde412b80787f6bc84277ed
AleixSaPa <sapafamilia@gmail.com>
2024-01-26 14:22:20 +0100

    Crear JavaScript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 var GolesNankatsu
+var QuePartidoQuieres = ('Que Partido Queres')
+var IfPartido1 = ('Quantos Goles As Marcado en el partido ');
+var IF4 = ('Eee numes i an tres partits')
+
+function QueQuieres() {
+    var Partido = prompt(QuePartidoQuieres)
+    if (Partido == 1) { var If1 = prompt(IfPartido1 + Partido) }
+    if (Partido == 2) { var If2 = prompt(IfPartido1 + Partido) }
+    if (Partido == 3) { var If3 = prompt(IfPartido1 + Partido) }
+    if (Partido = +4) { alert(IF4) }
+}
+
+
+
+
+function Console() {
+    console.log();
+}
