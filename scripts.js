b43a1c57fede7550074ea35a5b8bce8516c7c82f
AleixSaPa <sapafamilia@gmail.com>
2024-02-09 13:08:36 +0100

    Finicita El Projecte

--- scripts.js
+++ scripts.js
@@ -3,9 +3,7 @@ var StringPreguntaPartido = 'Cuantos Goles as marcado en el partido ';
 var StringPreguntaPartidoError = 'Eeeh! nomes hi han tres partits';
 var IntSumaGoles
 var DecPomedio
-var IntPartido1 = 0;
-var IntPartido2 = 0;
-var IntPartido3 = 0;
+var ArrayIntGols = [0, 0, 0];
 var QuansPartits = 0;
 function GolesMarcados() {
     var Partido = prompt(StringQuePartidoQuieres)
@@ -13,25 +11,25 @@ function GolesMarcados() {
 
 
     if (Partido == 1) {
-        if (IntPartido1 == 0) {
+        if (ArrayIntGols[0] == 0) {
             QuansPartits += 1;
         }
-        IntPartido1 = parseInt(prompt(StringQuestion));
+        ArrayIntGols[0] = parseInt(prompt(StringQuestion));
     } else if (Partido == 2) {
-        if (IntPartido2) {
+        if (ArrayIntGols[1] == 0) {
             QuansPartits += 1;
         }
-        IntPartido2 = parseInt(prompt(StringQuestion));
+        ArrayIntGols[1] = parseInt(prompt(StringQuestion));
     } else if (Partido == 3) {
-        if (IntPartido3) {
+        if (ArrayIntGols[2] == 0) {
             QuansPartits += 1;
         }
-        IntPartido3 = parseInt(prompt(StringQuestion))
+        ArrayIntGols[2] = parseInt(prompt(StringQuestion))
     } else {
         alert(StringPreguntaPartidoError)
     }
 
-    IntSumaGoles = IntPartido1 + IntPartido2 + IntPartido3;
+    IntSumaGoles = ArrayIntGols[0] + ArrayIntGols[1] + ArrayIntGols[2];
 
 
 }
